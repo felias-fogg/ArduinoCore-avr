5d4299717fa5aeee444052f45c75fbe25964c8d6
Bernhard Nebel <nebel@hinterm-ziel.de>
2026-07-07 17:02:36 +0200

    changed back to GCC7

--- Boards_manager_release.sh
+++ Boards_manager_release.sh
@@ -83,7 +83,7 @@ jq -r                                    \
 --arg file_name   $REPOSITORY-${DOWNLOADED_FILE#"v"}.tar.bz2  \
 '.packages[].platforms[.packages[].platforms | length] |= . +
 {
-  "name": "Arduino AVR Boards (GCC15)",
+  "name": "Arduino AVR Boards",
   "architecture": "avr",
   "version": $version,
   "category": "Contributed",
@@ -181,7 +181,7 @@ jq -r                                    \
     {
       "packager": "arduino",
       "name": "avr-gcc",
-      "version": "15.1.0-microchip4.0.0-01"
+      "version": "7.3.0-atmel3.6.1-arduino7"
     },
     {
       "packager": "arduino",
@@ -202,5 +202,4 @@ jq -r                                    \
 }' "package_${REALAUTHOR}_${REPOSITORY}_index.json.tmp" > "package_${REALAUTHOR}_${REPOSITORY}_index.json"
 
 # Remove files that's no longer needed
-rm -rf "$REPOSITORY-${DOWNLOADED_FILE#"v"}" "package_${REALAUTHOR}_${REPOSITORY}_index.json.tmp" "package_GCC15_${REPOSITORY}_index.json" 
-cp "package_${REALAUTHOR}_${REPOSITORY}_index.json" "package_GCC15_${REPOSITORY}_index.json" 
+rm -rf "$REPOSITORY-${DOWNLOADED_FILE#"v"}" "package_${REALAUTHOR}_${REPOSITORY}_index.json.tmp"
